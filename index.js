db422d048f647cb57ad69b25dd95a9f73efc97fd
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-28 21:35:14 +0600

    users insert api created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -9,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 //---------------------
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvjjrvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,8 +26,32 @@ async function run() {
     try {
         const menuCollection = client.db("restaurantBistro").collection('bistroMenu')
         const reviewsCollection = client.db("restaurantBistro").collection('bistroReviews')
+        const usersCollection = client.db("restaurantBistro").collection('users')
+        const cartCollection = client.db("restaurantBistro").collection('carts')
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        // creating jwt token
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
+            res.send({ token });
+        });
+
+        // save a user in db
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            // insert email if user doesn't exist:
+            // you can do this many ways (1. email unique, 2. upsert, 3. simple checking)
+            const query = { email: user.email };
+            const existInUsers = await usersCollection.findOne(query);
+            if (existInUsers) {
+                return res.send({ message: 'user already exist' });
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
         // menu api
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -36,7 +61,33 @@ async function run() {
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result);
-        })
+        });
+        // get all the cart data
+        app.get('/carts', async (req, res) => {
+            const email = req?.query?.email;
+            let query = {};
+            if (query) {
+                query = { email: email };
+            }
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // save a cart in db
+        app.post('/carts', async (req, res) => {
+            const cardItem = req.body;
+            console.log(cardItem);
+            const result = await cartCollection.insertOne(cardItem);
+            res.send(result);
+        });
+
+        // delete a cart
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
